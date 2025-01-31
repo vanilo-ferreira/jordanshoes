6f28ee8363ab1b700ef79c369e056d5957ab1785
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-01-30 23:25:28 -0300

    feat: Add banner content

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ function App() {
       <div>
         <p>Frete grátis para todo o Brasil</p>
       </div>
+
+      <div>
+        <p>JordanShoes</p>
+        <h4>A melhor loja de Jordan</h4>
+        <p>
+          O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o
+          jogador Michael Jordan.
+        </p>
+      </div>
     </>
   );
 }
