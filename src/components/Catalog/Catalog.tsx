bea02805de8c0eb67acc3a0b4437607f70db745f
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-02-06 00:01:14 -0300

    feat: Import Card component

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,10 +1,6 @@
-import jordan1 from "../../assets/jordan1.svg";
-import jordan2 from "../../assets/jordan2.svg";
-import jordan3 from "../../assets/jordan3.svg";
-import jordan4 from "../../assets/jordan4.svg";
-import jordan5 from "../../assets/jordan5.svg";
-import jordan6 from "../../assets/jordan6.svg";
+import { produtos } from "../../data/data";
 
+import { Card } from "../Card/Card";
 
 export function Catalog() {
     return (
@@ -18,99 +14,14 @@ export function Catalog() {
             </div>
 
             <div className="containerCards">
-                <div className="card">
-                    <div className="cardImage">
-                        <img
-                            className="image"
-                            src={jordan1}
-                            alt="Air Jordan 1 Mid Dutch Green"
-                        />
-                    </div>
-                    <div className="cardText">
-                        <p className="cardTitle">Air Jordan 1 Mid Dutch Green</p>
-                        <p className="cardDescription">Tênis Air Jordan</p>
-                        <p className="cardValue">R$ 1.199,99</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="cardImage">
-                        <img
-                            className="image"
-                            src={jordan2}
-                            alt="Air Jordan 1 High Zoom CMFT Tropical Twist"
-                        />
-                    </div>
-                    <div className="cardText">
-                        <p className="cardTitle">
-                            Air Jordan 1 High Zoom CMFT Tropical Twist
-                        </p>
-                        <p className="cardDescription">Tênis Air Jordan</p>
-                        <p className="cardValue">R$ 1.049,00</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="cardImage">
-                        <img
-                            className="image"
-                            src={jordan3}
-                            alt="Air Jordan 1 Mid Dutch Lilac"
-                        />
-                    </div>
-                    <div className="cardText">
-                        <p className="cardTitle">Air Jordan 1 Mid Dutch Lilac</p>
-                        <p className="cardDescription">Tênis Air Jordan</p>
-                        <p className="cardValue">R$ 1.350,00</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="cardImage">
-                        <img
-                            className="image"
-                            src={jordan4}
-                            alt='Air Jordan 1 Mid GS "Light Smoke Grey"'
-                        />
-                    </div>
-                    <div className="cardText">
-                        <p className="cardTitle">
-                            Air Jordan 1 Mid GS "Light Smoke Grey"
-                        </p>
-                        <p className="cardDescription">Tênis Air Jordan</p>
-                        <p className="cardValue">R$ 1.585,00</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="cardImage">
-                        <img
-                            className="image"
-                            src={jordan5}
-                            alt="Air Jordan 1 Mid SE Bright Citrus"
-                        />
-                    </div>
-                    <div className="cardText">
-                        <p className="cardTitle">Air Jordan 1 Mid SE Bright Citrus</p>
-                        <p className="cardDescription">Tênis Air Jordan</p>
-                        <p className="cardValue">R$ 949,99</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="cardImage">
-                        <img
-                            className="image"
-                            src={jordan6}
-                            alt="Air Jordan 1 Mid Grey Camo"
-                        />
-                    </div>
-                    <div className="cardText">
-                        <p className="cardTitle">Air Jordan 1 Mid Grey Camo</p>
-                        <p className="cardDescription">Tênis Air Jordan</p>
-                        <p className="cardValue">R$ 999,99</p>
-                    </div>
-                </div>
+                {produtos.map((produto) => (
+                    <Card
+                        name={produto.name}
+                        description={produto.description}
+                        value={produto.value}
+                        image={produto.image}
+                    />
+                ))};
             </div>
         </div>
     )
